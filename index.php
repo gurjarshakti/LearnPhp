8db242f519537d1a387b6be1869265ecaeefc863
gurjarshakti <mr.shakti.singh33@gmail.com>
2020-05-16 23:16:39 +0530

    Php Project

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+echo "<a href=/"path/variable.php/">Variable</a>";
+echo "<a href=/"path/phpwithhtml.php/">Variable</a>";
---
+++ variable.php
@@ -0,0 +1,14 @@
+<?php 
+$x = "Hello world!";
+$y = 'Hello world!';
+
+echo $x;
+echo "<br>"; 
+echo $y;
+?>
+
+<?php  
+$x = 5985;
+var_dump($x);
+?>  
+
